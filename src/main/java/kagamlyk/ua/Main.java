fcdc687ea6bab87061f6be3a12793fc2a8e7ccf3
Andrey Kahamlyk <kagamlykas@gmail.com>
2022-08-23 08:50:57 +0300

    Add getters in class Person

--- src/main/java/kagamlyk/ua/Main.java
+++ src/main/java/kagamlyk/ua/Main.java
@@ -12,4 +12,12 @@ public class Main {
 class Person{
     private int id;
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
